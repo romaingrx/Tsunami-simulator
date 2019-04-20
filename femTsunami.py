13162346f76cfac26086dd6bbc299a1f1a168b53
Romain Graux <romain.graux@student.uclouvain.be>
2019-04-20 16:11:36 +0200

    Ajout d'une classe Mesh, Edges et ProbTsunami avec des fonctions à l'intérieur - Pas encore propre

--- femTsunami.py
+++ femTsunami.py
@@ -1,13 +1,3 @@
-#
-# PYTHON for FEM DUMMIES 18-19
-# Projet "tsunami"
-#
-# Canevas de départ
-#  Vincent Legat
-#
-# -------------------------------------------------------------------------
-# 
-
 import numpy as np
 
 _gaussTri3Xsi    = np.array([0.166666666666667,0.666666666666667,0.166666666666667])
@@ -60,7 +50,7 @@ class Mesh(object):
             self.y     = self.xy[:,1] 
             self.lola  = np.array(list(list(np.array([np.arctan2(self.yStar[i],self.xStar[i])*180/np.pi,np.arcsin(self.zStar[i]/R)*180/np.pi]) for i in range(self.nNode))))
             self.longitude = self.lola[:,0]
-            self.latitude = self.lola[:,1]            
+            self.latitude = self.lola[:,1]                        
             
     def printf(self):
         print("Number of nodes %d" % self.nNode)
@@ -118,7 +108,7 @@ class Edges(object):
 
 # -------------------------------------------------------------------------
         
-class ProbTsunami(object):
+class Tsunami(object):
     
     def __init__(self,fileName,R,U,V,E):
         self.mesh   = Mesh(fileName,R);
@@ -135,95 +125,30 @@ class ProbTsunami(object):
                 f.write("%6d : %14.7e %14.7e %14.7e\n" % (i,*E[i,:]))
         print(" === iteration %6d : writing %s ===" % (iter,fileName))
         
-
-# -------------------------------------------------------------------------
-        
-def xStar(x,y,R):
-    return 4*R*R*x / (4*R*R + x*x + y*y)
-def yStar(x,y,R):
-    return  4*R*R*y / (4*R*R + x*x + y*y)
-def zStar(x,y,R):
-    return R*(4*R*R - x*x - y*y) / (4*R*R + x*x + y*y)
-def longitude(xStar,yStar):
-    return np.arctan2(yStar,xStar)*180/np.pi
-def latitude(zStar,R):
-    return np.arcsin(zStar/R)*180/np.pi
-
-# -------------------------------------------------------------------------
     
-def readMesh(fileName) :
-  with open(fileName,"r") as f :
-    nNode = int(f.readline().split()[3])
-    xyz   = np.array(list(list(float(w) for w in f.readline().split()[2:]) for i in range(nNode)))
-    nElem = int(f.readline().split()[3])
-    elem  = np.array(list(list(int(w)   for w in f.readline().split()[2:]) for i in range(nElem)))
-  X = xyz[:,0]
-  Y = xyz[:,1]
-  H = xyz[:,2] 
-  return [nNode,X,Y,H,nElem,elem]
-
-# -------------------------------------------------------------------------
-  
-def readResult(fileBaseName,iter,nElem) :
-  fileName = fileBaseName % iter
-  with open(fileName,"r") as f :
-    nSize = int(f.readline().split()[3])
-    if (nElem != nSize) :
-      print(" ==== Error : incoherent sizes : %d != %d" % (nElem,nSize))     
-    E = np.array(list(list(float(w) for w in f.readline().split()[2:5]) for i in range(nElem)))
-    print(" === iteration %6d : reading %s ===" % (iter,fileName))
-  return E
-
-# -------------------------------------------------------------------------
-
-def writeResult(fileBaseName,iter,E) :
-  fileName = fileBaseName % iter
-  nElem = E.shape[0]  
-  with open(fileName,"w") as f :
-    f.write("Number of elements %d\n" % nElem)
-    for i in range(nElem):
-      f.write("%6d : %14.7e %14.7e %14.7e\n" % (i,*E[i,:]))
-    print(" === iteration %6d : writing %s ===" % (iter,fileName))
     
- 
-# -------------------------------------------------------------------------
-
-def initialConditionOkada(x,y) :
-    R = 6371220;
-    x3d = 4*R*R*x / (4*R*R + x*x + y*y);
-    y3d = 4*R*R*y / (4*R*R + x*x + y*y);
-    z3d = R*(4*R*R - x*x - y*y) / (4*R*R + x*x + y*y);
-    lat = np.arcsin(z3d/R)*180/np.pi;
-    lon = np.arctan2(y3d,x3d)*180/np.pi;
-    lonMin = 142;
-    lonMax = 143.75;
-    latMin = 35.9;
-    latMax = 39.5;
-    olon = (lonMin+lonMax)/2;
-    olat = (latMin+latMax)/2;
-    angle = -12.95*np.pi/180; 
-    lon2 = olon + (lon-olon)*np.cos(angle) + (lat-olat)*np.sin(angle);
-    lat2 = olat - (lon-olon)*np.sin(angle) + (lat-olat)*np.cos(angle);
-    return np.all([lon2 <= lonMax,lon2 >= lonMin,lat2 >= latMin,lat2 <= latMax],axis=0).astype(int)
- 
-# -------------------------------------------------------------------------
-
-def compute(theFile,theResultFiles,U,V,E,dt,nIter,nSave):
-    R = 6371220
-    gamma = 10e-7;
-    g = 9.81;
-    omega = 2*np.pi / 86400;
-    Tsunami = ProbTsunami(theFile,R);
-    for t in range(nIter):
-        iterCompute(Tsunami);
-        if (t % dt == 0):
-            Tsunami.writeFile(theResultFiles % t)
-    return [Tsunami.U,Tsunami.V,Tsunami.E]
-
-# -------------------------------------------------------------------------
     
-def iterCompute(Tsunami):
-    return 1
-# -------------------------------------------------------------------------
-
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
